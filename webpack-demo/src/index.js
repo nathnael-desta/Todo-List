42b2e4f574971dc8d9fff3896b89f05003602ccf
Nathnael Desta <destanathnaelataro@gmail.com>
2023-11-17 15:30:25 +0300

    figure out no html loding on js problem with defer

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,18 +1,10 @@
 import './style.css';
 
-const front = document.getElementsByClassName("front");
-const dropdown = document.getElementsByClassName('dropdown');
+const front = document.querySelector(".create");
+const dropdown = document.querySelector('.dropdown');
 
-console.log(front);
-console.log(document)
-
-for (let i = 0; i < front.length; i++) {
-    front[i].addEventListener('click', toggleDropdown);
-  }
+front.addEventListener('click', toggleDropdown);
 
 function toggleDropdown() {
-    for (let i = 0; i < front.length; i++) {
-        dropdown[i].classList.toggle('show');
-      }   
+  
 }
-
